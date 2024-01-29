017b114aa2c537c90df5764dfd7a453cc5842866
sahil <123296616+sahil0-0-0@users.noreply.github.com>
2024-01-29 23:35:09 +0530

    Add files via upload

---
+++ Journal/Calculator/Calculator.Designer.vb
@@ -0,0 +1,348 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Calculator
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Btn0 = New Button()
+        Pntbtn = New Button()
+        Ansbtn = New Button()
+        Btn2 = New Button()
+        Btn1 = New Button()
+        Divbtn = New Button()
+        Btn3 = New Button()
+        Mulbtn = New Button()
+        Btn6 = New Button()
+        Btn5 = New Button()
+        Btn4 = New Button()
+        Minusbtn = New Button()
+        Btn9 = New Button()
+        Btn8 = New Button()
+        Btn7 = New Button()
+        Plusbtn = New Button()
+        Msign = New Button()
+        Clrbtn = New Button()
+        Scr = New TextBox()
+        Miniscr = New TextBox()
+        Acbtn = New Button()
+        SuspendLayout()
+        ' 
+        ' Btn0
+        ' 
+        Btn0.BackColor = Color.White
+        Btn0.Font = New Font("Yu Gothic UI", 22.2F)
+        Btn0.ForeColor = Color.Black
+        Btn0.Location = New Point(13, 510)
+        Btn0.Name = "Btn0"
+        Btn0.Size = New Size(80, 80)
+        Btn0.TabIndex = 0
+        Btn0.Text = "0"
+        Btn0.UseVisualStyleBackColor = False
+        ' 
+        ' Pntbtn
+        ' 
+        Pntbtn.BackColor = Color.White
+        Pntbtn.Font = New Font("Yu Gothic UI", 22.2F)
+        Pntbtn.ForeColor = Color.Black
+        Pntbtn.Location = New Point(99, 510)
+        Pntbtn.Name = "Pntbtn"
+        Pntbtn.Size = New Size(80, 80)
+        Pntbtn.TabIndex = 1
+        Pntbtn.Text = "."
+        Pntbtn.UseVisualStyleBackColor = False
+        ' 
+        ' Ansbtn
+        ' 
+        Ansbtn.BackColor = Color.White
+        Ansbtn.Font = New Font("Yu Gothic UI", 22.2F)
+        Ansbtn.ForeColor = Color.Black
+        Ansbtn.Location = New Point(185, 510)
+        Ansbtn.Name = "Ansbtn"
+        Ansbtn.Size = New Size(166, 80)
+        Ansbtn.TabIndex = 2
+        Ansbtn.Text = "="
+        Ansbtn.UseVisualStyleBackColor = False
+        ' 
+        ' Btn2
+        ' 
+        Btn2.BackColor = Color.White
+        Btn2.Font = New Font("Yu Gothic UI", 22.2F)
+        Btn2.ForeColor = Color.Black
+        Btn2.Location = New Point(99, 424)
+        Btn2.Name = "Btn2"
+        Btn2.Size = New Size(80, 80)
+        Btn2.TabIndex = 4
+        Btn2.Text = "2"
+        Btn2.UseVisualStyleBackColor = False
+        ' 
+        ' Btn1
+        ' 
+        Btn1.BackColor = Color.White
+        Btn1.Font = New Font("Yu Gothic UI", 22.2F)
+        Btn1.ForeColor = Color.Black
+        Btn1.Location = New Point(13, 424)
+        Btn1.Name = "Btn1"
+        Btn1.Size = New Size(80, 80)
+        Btn1.TabIndex = 3
+        Btn1.Text = "1"
+        Btn1.UseVisualStyleBackColor = False
+        ' 
+        ' Divbtn
+        ' 
+        Divbtn.BackColor = Color.White
+        Divbtn.Font = New Font("Yu Gothic UI", 22.2F)
+        Divbtn.ForeColor = Color.Black
+        Divbtn.Location = New Point(271, 424)
+        Divbtn.Name = "Divbtn"
+        Divbtn.Size = New Size(80, 80)
+        Divbtn.TabIndex = 6
+        Divbtn.Text = "/"
+        Divbtn.UseVisualStyleBackColor = False
+        ' 
+        ' Btn3
+        ' 
+        Btn3.BackColor = Color.White
+        Btn3.Font = New Font("Yu Gothic UI", 22.2F)
+        Btn3.ForeColor = Color.Black
+        Btn3.Location = New Point(185, 424)
+        Btn3.Name = "Btn3"
+        Btn3.Size = New Size(80, 80)
+        Btn3.TabIndex = 5
+        Btn3.Text = "3"
+        Btn3.UseVisualStyleBackColor = False
+        ' 
+        ' Mulbtn
+        ' 
+        Mulbtn.BackColor = Color.White
+        Mulbtn.Font = New Font("Yu Gothic UI", 22.2F)
+        Mulbtn.ForeColor = Color.Black
+        Mulbtn.Location = New Point(271, 338)
+        Mulbtn.Name = "Mulbtn"
+        Mulbtn.Size = New Size(80, 80)
+        Mulbtn.TabIndex = 10
+        Mulbtn.Text = "x"
+        Mulbtn.UseVisualStyleBackColor = False
+        ' 
+        ' Btn6
+        ' 
+        Btn6.BackColor = Color.White
+        Btn6.Font = New Font("Yu Gothic UI", 22.2F)
+        Btn6.ForeColor = Color.Black
+        Btn6.Location = New Point(185, 338)
+        Btn6.Name = "Btn6"
+        Btn6.Size = New Size(80, 80)
+        Btn6.TabIndex = 9
+        Btn6.Text = "6"
+        Btn6.UseVisualStyleBackColor = False
+        ' 
+        ' Btn5
+        ' 
+        Btn5.BackColor = Color.White
+        Btn5.Font = New Font("Yu Gothic UI", 22.2F)
+        Btn5.ForeColor = Color.Black
+        Btn5.Location = New Point(99, 338)
+        Btn5.Name = "Btn5"
+        Btn5.Size = New Size(80, 80)
+        Btn5.TabIndex = 8
+        Btn5.Text = "5"
+        Btn5.UseVisualStyleBackColor = False
+        ' 
+        ' Btn4
+        ' 
+        Btn4.BackColor = Color.White
+        Btn4.Font = New Font("Yu Gothic UI", 22.2F)
+        Btn4.ForeColor = Color.Black
+        Btn4.Location = New Point(13, 338)
+        Btn4.Name = "Btn4"
+        Btn4.Size = New Size(80, 80)
+        Btn4.TabIndex = 7
+        Btn4.Text = "4"
+        Btn4.UseVisualStyleBackColor = False
+        ' 
+        ' Minusbtn
+        ' 
+        Minusbtn.BackColor = Color.White
+        Minusbtn.Font = New Font("Yu Gothic UI", 22.2F)
+        Minusbtn.ForeColor = Color.Black
+        Minusbtn.Location = New Point(271, 252)
+        Minusbtn.Name = "Minusbtn"
+        Minusbtn.Size = New Size(80, 80)
+        Minusbtn.TabIndex = 14
+        Minusbtn.Text = "-"
+        Minusbtn.UseVisualStyleBackColor = False
+        ' 
+        ' Btn9
+        ' 
+        Btn9.BackColor = Color.White
+        Btn9.Font = New Font("Yu Gothic UI", 22.2F)
+        Btn9.ForeColor = Color.Black
+        Btn9.Location = New Point(185, 252)
+        Btn9.Name = "Btn9"
+        Btn9.Size = New Size(80, 80)
+        Btn9.TabIndex = 13
+        Btn9.Text = "9"
+        Btn9.UseVisualStyleBackColor = False
+        ' 
+        ' Btn8
+        ' 
+        Btn8.BackColor = Color.White
+        Btn8.Font = New Font("Yu Gothic UI", 22.2F)
+        Btn8.ForeColor = Color.Black
+        Btn8.Location = New Point(99, 252)
+        Btn8.Name = "Btn8"
+        Btn8.Size = New Size(80, 80)
+        Btn8.TabIndex = 12
+        Btn8.Text = "8"
+        Btn8.UseVisualStyleBackColor = False
+        ' 
+        ' Btn7
+        ' 
+        Btn7.BackColor = Color.White
+        Btn7.Font = New Font("Yu Gothic UI", 22.2F)
+        Btn7.ForeColor = Color.Black
+        Btn7.Location = New Point(13, 252)
+        Btn7.Name = "Btn7"
+        Btn7.Size = New Size(80, 80)
+        Btn7.TabIndex = 11
+        Btn7.Text = "7"
+        Btn7.UseVisualStyleBackColor = False
+        ' 
+        ' Plusbtn
+        ' 
+        Plusbtn.BackColor = Color.White
+        Plusbtn.Font = New Font("Yu Gothic UI", 22.2F)
+        Plusbtn.ForeColor = Color.Black
+        Plusbtn.Location = New Point(271, 166)
+        Plusbtn.Name = "Plusbtn"
+        Plusbtn.Size = New Size(80, 80)
+        Plusbtn.TabIndex = 18
+        Plusbtn.Text = "+"
+        Plusbtn.UseVisualStyleBackColor = False
+        ' 
+        ' Msign
+        ' 
+        Msign.BackColor = Color.White
+        Msign.Font = New Font("Yu Gothic UI", 22.2F)
+        Msign.ForeColor = Color.Black
+        Msign.Location = New Point(185, 166)
+        Msign.Name = "Msign"
+        Msign.Size = New Size(80, 80)
+        Msign.TabIndex = 16
+        Msign.Text = "±"
+        Msign.UseVisualStyleBackColor = False
+        ' 
+        ' Clrbtn
+        ' 
+        Clrbtn.BackColor = Color.White
+        Clrbtn.Font = New Font("Yu Gothic UI", 22.2F)
+        Clrbtn.ForeColor = Color.Black
+        Clrbtn.Location = New Point(99, 166)
+        Clrbtn.Name = "Clrbtn"
+        Clrbtn.Size = New Size(80, 80)
+        Clrbtn.TabIndex = 15
+        Clrbtn.Text = "C"
+        Clrbtn.UseVisualStyleBackColor = False
+        ' 
+        ' Scr
+        ' 
+        Scr.Font = New Font("Yu Gothic UI", 36F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Scr.Location = New Point(13, 60)
+        Scr.Name = "Scr"
+        Scr.Size = New Size(338, 87)
+        Scr.TabIndex = 19
+        Scr.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Miniscr
+        ' 
+        Miniscr.Font = New Font("Yu Gothic UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Miniscr.Location = New Point(12, 12)
+        Miniscr.Name = "Miniscr"
+        Miniscr.Size = New Size(338, 31)
+        Miniscr.TabIndex = 20
+        Miniscr.TextAlign = HorizontalAlignment.Right
+        ' 
+        ' Acbtn
+        ' 
+        Acbtn.BackColor = Color.White
+        Acbtn.Font = New Font("Yu Gothic UI", 22.2F)
+        Acbtn.ForeColor = Color.Black
+        Acbtn.Location = New Point(13, 166)
+        Acbtn.Name = "Acbtn"
+        Acbtn.Size = New Size(80, 80)
+        Acbtn.TabIndex = 21
+        Acbtn.Text = "AC"
+        Acbtn.UseVisualStyleBackColor = False
+        ' 
+        ' Calculator
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.WhiteSmoke
+        ClientSize = New Size(360, 604)
+        Controls.Add(Acbtn)
+        Controls.Add(Miniscr)
+        Controls.Add(Scr)
+        Controls.Add(Plusbtn)
+        Controls.Add(Msign)
+        Controls.Add(Clrbtn)
+        Controls.Add(Minusbtn)
+        Controls.Add(Btn9)
+        Controls.Add(Btn8)
+        Controls.Add(Btn7)
+        Controls.Add(Mulbtn)
+        Controls.Add(Btn6)
+        Controls.Add(Btn5)
+        Controls.Add(Btn4)
+        Controls.Add(Divbtn)
+        Controls.Add(Btn3)
+        Controls.Add(Btn2)
+        Controls.Add(Btn1)
+        Controls.Add(Ansbtn)
+        Controls.Add(Pntbtn)
+        Controls.Add(Btn0)
+        Name = "Calculator"
+        Text = "Calculator"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Btn0 As Button
+    Friend WithEvents Pntbtn As Button
+    Friend WithEvents Ansbtn As Button
+    Friend WithEvents Btn2 As Button
+    Friend WithEvents Btn1 As Button
+    Friend WithEvents Divbtn As Button
+    Friend WithEvents Btn3 As Button
+    Friend WithEvents Mulbtn As Button
+    Friend WithEvents Btn6 As Button
+    Friend WithEvents Btn5 As Button
+    Friend WithEvents Btn4 As Button
+    Friend WithEvents Minusbtn As Button
+    Friend WithEvents Btn9 As Button
+    Friend WithEvents Btn8 As Button
+    Friend WithEvents Btn7 As Button
+    Friend WithEvents Plusbtn As Button
+    Friend WithEvents Msign As Button
+    Friend WithEvents Clrbtn As Button
+    Friend WithEvents Scr As TextBox
+    Friend WithEvents Miniscr As TextBox
+    Friend WithEvents Acbtn As Button
+End Class
---
+++ Journal/Calculator/Calculator.vb
@@ -0,0 +1,126 @@
+﻿Imports System.Reflection.Metadata
+
+Public Class Calculator
+    Dim sign As String
+    Dim value As Double
+    Private Sub Btn1_Click(sender As Object, e As EventArgs) Handles Btn1.Click
+        Scr.Text = Scr.Text + "1"
+        Msign.Enabled = False
+    End Sub
+
+    Private Sub Btn2_Click(sender As Object, e As EventArgs) Handles Btn2.Click
+        Scr.Text = Scr.Text + "2"
+        Msign.Enabled = False
+    End Sub
+
+    Private Sub Btn3_Click(sender As Object, e As EventArgs) Handles Btn3.Click
+        Scr.Text = Scr.Text + "3"
+        Msign.Enabled = False
+    End Sub
+
+    Private Sub Btn4_Click(sender As Object, e As EventArgs) Handles Btn4.Click
+        Scr.Text = Scr.Text + "4"
+        Msign.Enabled = False
+    End Sub
+
+    Private Sub Btn5_Click(sender As Object, e As EventArgs) Handles Btn5.Click
+        Scr.Text = Scr.Text + "5"
+        Msign.Enabled = False
+    End Sub
+
+    Private Sub Btn6_Click(sender As Object, e As EventArgs) Handles Btn6.Click
+        Scr.Text = Scr.Text + "6"
+        Msign.Enabled = False
+    End Sub
+
+    Private Sub Btn7_Click(sender As Object, e As EventArgs) Handles Btn7.Click
+        Scr.Text = Scr.Text + "7"
+        Msign.Enabled = False
+    End Sub
+
+    Private Sub Btn8_Click(sender As Object, e As EventArgs) Handles Btn8.Click
+        Scr.Text = Scr.Text + "8"
+        Msign.Enabled = False
+    End Sub
+
+    Private Sub Btn9_Click(sender As Object, e As EventArgs) Handles Btn9.Click
+        Scr.Text = Scr.Text + "9"
+        Msign.Enabled = False
+    End Sub
+
+    Private Sub Btn0_Click(sender As Object, e As EventArgs) Handles Btn0.Click
+        Scr.Text = Scr.Text + "0"
+        Msign.Enabled = False
+    End Sub
+
+    Private Sub Pntbtn_Click(sender As Object, e As EventArgs) Handles Pntbtn.Click
+        Scr.Text = Scr.Text + "."
+        Pntbtn.Enabled = False
+    End Sub
+
+    Private Sub Msign_Click(sender As Object, e As EventArgs) Handles Msign.Click
+        Scr.Text = Scr.Text + "-"
+        Msign.Enabled = False
+    End Sub
+
+    Private Sub Clrbtn_Click(sender As Object, e As EventArgs) Handles Clrbtn.Click
+        Scr.Clear()
+        Pntbtn.Enabled = True
+        Msign.Enabled = True
+    End Sub
+
+    Private Sub Acbtn_Click(sender As Object, e As EventArgs) Handles Acbtn.Click
+        Scr.Clear()
+        Miniscr.Clear()
+        Pntbtn.Enabled = True
+        Msign.Enabled = True
+    End Sub
+
+    Private Sub Plusbtn_Click(sender As Object, e As EventArgs) Handles Plusbtn.Click
+        sign = "+"
+        Display()
+    End Sub
+
+    Private Sub Minusbtn_Click(sender As Object, e As EventArgs) Handles Minusbtn.Click
+        sign = "-"
+        Display()
+    End Sub
+
+    Private Sub Mulbtn_Click(sender As Object, e As EventArgs) Handles Mulbtn.Click
+        sign = "x"
+        Display()
+    End Sub
+
+    Private Sub Divbtn_Click(sender As Object, e As EventArgs) Handles Divbtn.Click
+        sign = "/"
+        Display()
+    End Sub
+
+    Public Sub Display()
+        value = Scr.Text
+        Miniscr.Text = Miniscr.Text & " = " & CStr(value) & " " & CStr(sign) & " "
+        Scr.Clear()
+        Scr.Focus()
+        Pntbtn.Enabled = True
+        Msign.Enabled = True
+    End Sub
+
+    Private Sub Ansbtn_Click(sender As Object, e As EventArgs) Handles Ansbtn.Click
+        Pntbtn.Enabled = True
+        Msign.Enabled = True
+
+        Miniscr.Text = Miniscr.Text + Scr.Text
+
+        If sign = "+" Then
+            Scr.Text = value + Val(Scr.Text)
+        ElseIf sign = "-" Then
+            Scr.Text = value - Val(Scr.Text)
+        ElseIf sign = "x" Then
+            Scr.Text = value * Val(Scr.Text)
+        ElseIf sign = "/" Then
+            Scr.Text = value / Val(Scr.Text)
+        End If
+    End Sub
+
+
+End Class
