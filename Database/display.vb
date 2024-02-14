f0dace0ae95e931a01a323ae8e8261df900a68bf
sahil <123296616+sahil0-0-0@users.noreply.github.com>
2024-02-15 03:02:14 +0530

    Add files via upload

---
+++ Database/display.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Data.OleDb
+Public Class Form1
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim con As New OleDbConnection
+        Dim cmd As New OleDbCommand
+        Dim qry As String
+        con.ConnectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\sahil\OneDrive\Desktop\mdbFile\Demo.mdb"
+        con.Open()
+        cmd.Connection = con
+        qry = "select * from student"
+        Dim da As New OleDbDataAdapter(qry, con)
+        Dim dt As New DataTable
+        da.Fill(dt)
+        DataGridView1.DataSource = dt
+    End Sub
+End Class
